1f934dadf87ae832afa87b54144bfae74ad9c4b5
mikeyolang <brunomike965@gmail.com>
2023-09-13 10:08:06 +0300

    Edited the snack bar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -44,6 +44,22 @@ class ProductItem extends StatelessWidget {
             ),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text(
+                    'Added item to cart!',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).primaryColor,
           ),
